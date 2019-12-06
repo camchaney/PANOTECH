e738e7a1ec0a806ca2c4d368d9b40662e5c916f9
Alyssa Yang <alyssayang@airbears2-10-142-36-157.airbears2.1918.berkeley.edu>
2019-12-05 19:19:47 -0800

    yababa

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ let win
 function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 960,
+    height: 1280,
     webPreferences: {
       nodeIntegration: true
     }
